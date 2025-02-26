19ce792c8551a37f02c84dd863466d13b741725b
Dylan Schultz <9121234+dylanschultzie@users.noreply.github.com>
2025-02-26 11:07:38 -0800

    Pass in log level
    Signed-off-by: Dylan Schultz <9121234+dylanschultzie@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
 import logging
 import sys
+import argparse
 from flask import Flask
 from app.routes import api  # Import the Blueprint
 
 sys.stdout.reconfigure(line_buffering=True)
-
-logging.basicConfig(level=logging.INFO, format="%(levelname)s | %(asctime)s | %(message)s")
 
 app = Flask(__name__)
 app.register_blueprint(api)  # Register the Blueprint
 
 logger = logging.getLogger(__name__)
 
-
 def main():
+    parser = argparse.ArgumentParser(description="Run the Flask application.")
+    parser.add_argument("--log-level", default="INFO", help="Set the logging level (default: INFO)")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level.upper(), format="%(levelname)s | %(asctime)s | %(message)s")
+
     app.run(host="0.0.0.0", port=53336)
 
-
-main()
+if __name__ == "__main__":
+    main()
